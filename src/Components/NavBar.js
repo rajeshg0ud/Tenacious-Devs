a070c9067d80f9e7b9265e95813e6e60e4685fb2
unknown <budidirajeshgoud231@gmail.com>
2025-02-16 22:58:33 +0530

    font uodate

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import tenacious from "../Assets/Tenacious.png";
+import tenaciousfav from "../Assets/Tenacious-fav.png";
+
 function NavBar() {
   const scrollDownTo = (sectionId) => {
     const section = document.getElementById(sectionId);
@@ -15,29 +17,38 @@ function NavBar() {
         {/* <h1 className=" hidden md:block text-base md:text-2xl carter-one-regular text-sky-600 brightness-110">
           Tenacious <span className="text-black">Devs</span>
         </h1> */}
-        <img src={tenacious} alt="logo" className="max-w-44 hidden md:block " />
+        <img
+          src={tenacious}
+          alt="logo"
+          className="max-w-44  hidden md:block "
+        />
+        <img
+          src={tenaciousfav}
+          alt="logo"
+          className="max-w-10 block  md:hidden"
+        />
       </div>
       <div className=" mx-auto md:mx-0 flex space-x-5 md:space-x-14 font-semibold vollkorn">
         <h1
-          className="hover:text-sky-600 text-[14px] md:text-[14.5px]  cursor-pointer"
+          className="hover:text-sky-600 text-[10px] md:text-[14.5px]  cursor-pointer"
           onClick={() => scrollDownTo("Home")}
         >
           HOME
         </h1>
         <h1
-          className="hover:text-sky-600 text-[14px] md:text-[14.5px] cursor-pointer"
+          className="hover:text-sky-600 text-[10px] md:text-[14.5px] cursor-pointer"
           onClick={() => scrollDownTo("Services")}
         >
           SERVICES
         </h1>
         <h1
-          className="hover:text-sky-600 text-[14px] md:text-[14.5px] cursor-pointer"
+          className="hover:text-sky-600 text-[10px] md:text-[14.5px] cursor-pointer"
           onClick={() => scrollDownTo("Projects")}
         >
           PROJECTS
         </h1>
         <h1
-          className="hover:text-sky-600 text-[14px] md:text-[14.5px] cursor-pointer"
+          className="hover:text-sky-600 text-[10px] md:text-[14.5px] cursor-pointer"
           onClick={() => scrollDownTo("Contact")}
         >
           CONTACT US
