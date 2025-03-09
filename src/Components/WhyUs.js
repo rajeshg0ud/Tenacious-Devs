7d795e3b5e0970f32eb254348dee256739edb651
unknown <budidirajeshgoud231@gmail.com>
2025-03-09 15:52:08 +0530

    assets update

--- src/Components/WhyUs.js
+++ src/Components/WhyUs.js
@@ -3,7 +3,7 @@ import GRAPHICDESIGN from "../Assets/GRAPHICDESIGN.png";
 import WEBDEV from "../Assets/WEBDEV.png";
 import WEBREVAMP from "../Assets/WEBREVAMP.png";
 import CUSTOMSOFT from "../Assets/CUSTOMSOFT.png";
-import UIUXw from "../Assets/UIUXw.png";
+import UIUX from "../Assets/UIUX.png";
 
 function WhyUs() {
   const services = [
@@ -21,7 +21,7 @@ function WhyUs() {
     },
     {
       name: "UI/UX Design",
-      image: UIUXw,
+      image: UIUX,
       description:
         "Engaging and user-friendly interfaces designed for seamless digital experiences.",
     },
